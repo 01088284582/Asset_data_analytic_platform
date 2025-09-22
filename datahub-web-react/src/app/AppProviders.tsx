9dcfd2533a08eeac3c48f9645856071915540a19
gyudokim <gyudokim@mz.co.kr>
2025-09-22 15:10:09 +0900

    20250922-01

---
+++ datahub-web-react/src/app/AppProviders.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import EntityRegistryProvider from '@app/EntityRegistryProvider';
+import GlobalSettingsProvider from '@app/context/GlobalSettingsProvider';
+import UserContextProvider from '@app/context/UserContextProvider';
+import { NavBarProvider } from '@app/homeV2/layout/navBarRedesign/NavBarContext';
+import HomePageProvider from '@app/homeV3/context/HomePageProvider';
+import { ModulesProvider } from '@app/homeV3/module/context/ModulesContext';
+import OnboardingTourProvider from '@app/onboarding/OnboardingTourContextProvider';
+import SearchContextProvider from '@app/search/context/SearchContextProvider';
+import { BrowserTitleProvider } from '@app/shared/BrowserTabTitleContext';
+import { EducationStepsProvider } from '@providers/EducationStepsProvider';
+import QuickFiltersProvider from '@providers/QuickFiltersProvider';
+import AppConfigProvider from '@src/AppConfigProvider';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function AppProviders({ children }: Props) {
+    return (
+        <AppConfigProvider>
+            <GlobalSettingsProvider>
+                <UserContextProvider>
+                    <EntityRegistryProvider>
+                        <BrowserTitleProvider>
+                            <EducationStepsProvider>
+                                <OnboardingTourProvider>
+                                    <QuickFiltersProvider>
+                                        <SearchContextProvider>
+                                            <ModulesProvider>
+                                                <HomePageProvider>
+                                                    <NavBarProvider>{children}</NavBarProvider>
+                                                </HomePageProvider>
+                                            </ModulesProvider>
+                                        </SearchContextProvider>
+                                    </QuickFiltersProvider>
+                                </OnboardingTourProvider>
+                            </EducationStepsProvider>
+                        </BrowserTitleProvider>
+                    </EntityRegistryProvider>
+                </UserContextProvider>
+            </GlobalSettingsProvider>
+        </AppConfigProvider>
+    );
+}
